f1cdb9471aaba0cc2421c7412d5e8c7d5a98690c
Yuji Takaesu <taka.esu.user@gmail.com>
2014-06-19 12:54:36 +0900

    complete answer

--- lib/karaoke_machine.rb
+++ lib/karaoke_machine.rb
@@ -1,10 +1,12 @@
 # coding: utf-8
 
 class KaraokeMachine
-  attr_accessor :melody
-  MUSICAL_SCALES = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"].freeze
+  ONE_OCTAVE_SCALES = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"].freeze
+  ONE_OCTAVE_SIZE = ONE_OCTAVE_SCALES.size
   REST_NOTE = " "
-  A_BAR_REGEXP = /(\s|[CDEFGA]#?)/.freeze
+  A_BAR_REGEXP = /(\s|[CDEFGAB]#?)/.freeze
+
+  attr_accessor :melody
 
   def initialize(melody)
     @melody = melody
@@ -16,14 +18,20 @@ class KaraokeMachine
 
   def transpose_each_a_bar(a_bar, amount)
     split_to_notes(a_bar).map {|note|
-      note == REST_NOTE ? REST_NOTE : MUSICAL_SCALES[MUSICAL_SCALES.index(note) + amount]
+      if note == REST_NOTE
+        REST_NOTE
+      else
+        # 負の数の場合に関してもわざわざ絶対値を使わなくても余りが次の範囲になるので添字でアクセスするので問題ない
+        # [0 <= 余り < 割る数]となるため
+        transposed_val = (ONE_OCTAVE_SCALES.index(note) + amount) % ONE_OCTAVE_SIZE
+        ONE_OCTAVE_SCALES[transposed_val]
+      end
     }.join("")
   end
 
   def split_to_notes(a_bar_melody)
-    # A_BAR_REGEXPの正規表現で分割するので無駄な空白ができてしまうのを取り除く
+    # selectメソッドで空白を除いているのは、
+    # A_BAR_REGEXPの正規表現で分割するので無駄な空白ができてしまうため
     a_bar_melody.split(A_BAR_REGEXP).select{|v| !v.empty?}
   end
-
 end
-
