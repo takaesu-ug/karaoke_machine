158f37e5c42d23ac7264ad057b86152135898425
Yuji Takaesu <taka.esu.user@gmail.com>
2014-06-29 23:51:18 +0900

    refactoring after receive evaluation
    - exclude unnecessary split method by a_bar
    - make good use of Regular Expression

--- lib/karaoke_machine.rb
+++ lib/karaoke_machine.rb
@@ -1,37 +1,33 @@
 # coding: utf-8
 
 class KaraokeMachine
-  ONE_OCTAVE_SCALES = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"].freeze
-  ONE_OCTAVE_SIZE = ONE_OCTAVE_SCALES.size
   REST_NOTE = " "
-  A_BAR_REGEXP = /(\s|[CDEFGAB]#?)/.freeze
+  A_BAR_SPLIT = "|"
+  ONE_OCTAVE_SCALES = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"].freeze
+  NOTE_REGEXP = /\s|\||[CDEFGAB]#?/.freeze # 空白, |, [CDEFGAB]#? の文字を定義
+  NOT_TRANSPOSE_REGEXP = Regexp.new("#{REST_NOTE}|\\#{A_BAR_SPLIT}").freeze  #eq  / |\|/.freeze
 
-  attr_accessor :melody
+  attr_reader :melody
 
   def initialize(melody)
     @melody = melody
   end
 
   def transpose(amount)
-    melody.split("|").map {|a_bar| transpose_each_a_bar(a_bar, amount) }.join("|")
-  end
-
-  def transpose_each_a_bar(a_bar, amount)
-    split_to_notes(a_bar).map {|note|
-      if note == REST_NOTE
-        REST_NOTE
+    # refs. http://docs.ruby-lang.org/ja/1.8.7/class/String.html#I_SCAN
+    melody.scan(NOTE_REGEXP).map {|note|
+      if not_transpose_note?(note)
+        note
       else
-        # 負の数の場合に関してもわざわざ絶対値を使わなくても余りが次の範囲になるので添字でアクセスするので問題ない
-        # [0 <= 余り < 割る数]となるため
-        transposed_val = (ONE_OCTAVE_SCALES.index(note) + amount) % ONE_OCTAVE_SIZE
+        # 負の数の場合に関してもわざわざ絶対値を使わなくても余りが次の範囲になり添字でアクセスするので問題ない
+        # [0 <= 余り < 割る数]となるため(余りは整数となる)
+        transposed_val = (ONE_OCTAVE_SCALES.index(note) + amount) % ONE_OCTAVE_SCALES.size
         ONE_OCTAVE_SCALES[transposed_val]
       end
     }.join("")
   end
 
-  def split_to_notes(a_bar_melody)
-    # selectメソッドで空白を除いているのは、
-    # A_BAR_REGEXPの正規表現で分割するので無駄な空白ができてしまうため
-    a_bar_melody.split(A_BAR_REGEXP).select{|v| !v.empty?}
+  def not_transpose_note?(note)
+    note =~ NOT_TRANSPOSE_REGEXP
   end
 end
