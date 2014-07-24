9f12182efe4e9edf829b4c791ebc133c58ae376b
Yuji Takaesu <taka.esu.user@gmail.com>
2014-07-24 12:48:22 +0900

    refactoring and add reference code, add README explaination,  after session

--- README.md
+++ README.md
@@ -1,11 +1,26 @@
 KARAOKE MACHINE QUESTION
 ===========================
 
+放送後のメモ
+---------------------------
+[予備知識が無いと理解しづらかったり、意味づけが人によって異なったりするようなプログラム設計は避けるべき、というお話 #sg_study - give IT a try](http://blog.jnito.com/entry/2014/07/11/111813)
+
+  gsubメソッドでやる /[A-G]#?/
+  オライリー正規表現の本
+
+   いちいちscanで配列にしなくても
+   String gsub メソッドで置換してあげる
+   Array rotateメソッド、zipメソッド, transposeメソッド ネストされた配列をto_hするとネスト部分がハッシュになる
+   gsubすげー
+   第一引数に正規表現、第二引数に変換用のハッシュを渡すとキーに応じたからバリューに変換される
+
+   ***自分の書いた正規表現使わないコードとベンチマーク比べてみたら，正規表現でgsubってめちゃくちゃ速いな #sg_study***
+
+
 Question
 ---------------------------
 [CodeIQに「カラオケマシン問題」を出題しました。みんなチャレンジしてね！ - give IT a try](http://blog.jnito.com/entry/2014/06/06/104420)
 
-参考
 
 ```
 ド ド# レ レ# ミ ファ ファ# ソ ソ# ラ ラ# シ
@@ -21,6 +36,8 @@ git archive --format=zip HEAD --prefix=answer/ -o answer.zip
 
 Run
 ------------------------
-
 1. `bundle install`
 2. `bundle exec rake spec` (OR `bundle exec rspec spec/` )
+
+
+
--- lib/karaoke_machine.rb
+++ lib/karaoke_machine.rb
@@ -1,12 +1,9 @@
 # coding: utf-8
 
 class KaraokeMachine
-  REST_NOTE = " "
-  A_BAR_SPLIT = "|"
-  ONE_OCTAVE_SCALES = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"].freeze
-  NOTE_REGEXP = /\s|\||[CDEFGAB]#?/.freeze # 空白, |, [CDEFGAB]#? の文字を定義
-  NOT_TRANSPOSE_REGEXP = Regexp.new("#{REST_NOTE}|\\#{A_BAR_SPLIT}").freeze  #eq  / |\|/.freeze
-
+  ONE_OCTAVE_SCALES = %w(C C# D D# E F F# G G# A A# B).freeze
+  NOTE_REGEXP = / |\||[CDEFGAB]#?/.freeze # 空白, |, [CDEFGAB]#? の文字を定義
+  NOT_TRANSPOSE_REGEXP = / |\|/.freeze
   attr_reader :melody
 
   def initialize(melody)
@@ -21,13 +18,35 @@ class KaraokeMachine
       else
         # 負の数の場合に関してもわざわざ絶対値を使わなくても余りが次の範囲になり添字でアクセスするので問題ない
         # [0 <= 余り < 割る数]となるため(余りは整数となる)
-        transposed_val = (ONE_OCTAVE_SCALES.index(note) + amount) % ONE_OCTAVE_SCALES.size
-        ONE_OCTAVE_SCALES[transposed_val]
+        transposed_note_index = (ONE_OCTAVE_SCALES.index(note) + amount) % ONE_OCTAVE_SCALES.size
+        ONE_OCTAVE_SCALES[transposed_note_index]
       end
     }.join("")
   end
 
   def not_transpose_note?(note)
     note =~ NOT_TRANSPOSE_REGEXP
+  end
+end
+
+class KaraokeMachineGsub
+  SCALES = %w(C C# D D# E F F# G G# A A# B).freeze
+
+  attr_reader :melody
+  def initialize(melody)
+    @melody = melody
+  end
+
+  def transpose(amount)
+    melody.gsub(/[A-G]#?/) do |m|
+      move_index = (SCALES.index(m) + amount) % SCALES.size
+      SCALES[move_index]
+    end
+  end
+
+  def transpose_exemplary(amount)
+    # 変換テーブル converter を作ってgsubにハッシュを渡して変換する
+    converter = [SCALES, SCALES.rotate(amount)].transpose.to_h
+    melody.gsub(/[A-G]#?/, converter)
   end
 end
